82fc865cbf2d3355a6af61e4e9148381136d0ea3
matejmeglic <matej@matejmeglic.com>
2020-07-07 22:32:34 +0200

    SEO 5

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -68,7 +68,7 @@ const BlogPostTemplate = ({ data }) => {
  
   return (
   <div>
-      <SEO title={post.frontmatter.title} description={post.excerpt} lang2={language}/>
+      <SEO title={post.frontmatter.title} description={post.excerpt} lang={language}/>
       <Layout title={siteTitle} />
       <Maps geoJsonData={jsonData} />
       <div className="post">
